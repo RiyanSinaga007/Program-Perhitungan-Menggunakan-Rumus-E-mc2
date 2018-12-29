9af46efffaa49135a2ef38c4f82c08778e1b74f6
RiyanSinaga007 <45660151+RiyanSinaga007@users.noreply.github.com>
2018-12-30 01:01:03 +0700

    Add files via upload

---
+++ Program Perhitungan Menggunakan Rumus E=mc2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int();
+	float c=89.88,m,E;
+	printf("PROGRAM PERHITUNGAN MENGGUNAKAN RUMUS E=mc2 \n");
+	printf("--------------------\n");
+	printf("Masukan massa (kg):");scanf("%f",&m);
+	E=m*c*c;
+	printf("E= %.2f * %.2f * %.2f = %.2f J",m,c,c,E);
+	getch();
+}
